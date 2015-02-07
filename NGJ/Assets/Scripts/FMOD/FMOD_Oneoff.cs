5efdc13e255df64987a04cf0be22f3cc5fa28f66
Stibbins <john.spjuth@gmail.com>
2015-02-07 01:38:02 +0100

    FMOD package

---
+++ NGJ/Assets/Scripts/FMOD/FMOD_Oneoff.cs
@@ -0,0 +1,24 @@
+﻿ using UnityEngine;
+using System.Collections;
+using FMOD.Studio;
+
+public class FMOD_Oneoff : MonoBehaviour {
+
+	[SerializeField] public bool _bPlaySound = true;
+	[SerializeField] private FMOD.Studio.EventInstance _soundEvent;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+		if (_bPlaySound == true){
+			FMOD_StudioSystem.instance.PlayOneShot(_soundEvent, transform.position)
+		}
+	
+	}
+}
